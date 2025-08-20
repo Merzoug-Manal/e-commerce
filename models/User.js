b8e2cb6d6b7896987b45be426171c017614f2919
Merzoug-Manal <127239819+Merzoug-Manal@users.noreply.github.com>
2025-08-20 19:07:34 +0100

    fix error

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id:{type:String, required: true},
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    imageUrl :{type: String, required: true},
-    cartItems :{type:Object,default: {}},
-},{minimize: false});
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+}, { minimize: false });
 
-const User = mongoose.models|| mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
